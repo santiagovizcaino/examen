d9cda520f1a637486adb90a58b5ccd6c6b47d8a5
santiago <jvizcainot@est.ups.edu.ec>
2019-11-15 12:55:24 -0500

    mostrar

--- buscador/buscar.js
+++ buscador/buscar.js
@@ -1,9 +1,20 @@
 const fs = require('fs')
 const csv = require('csv-parser');
 let mostrar = () => {
+    busca = 'ZWE';
+    let csvData = [];
     let readstream = fs.createReadStream('datos.csv').pipe(csv());
-    readstream.on("data", (data) => {
-        console.log(data);
+    readstream.on("data", function(csvrow) {
+        console.log(csvrow);
+        //do something with csvrow
+        csvData.push(csvrow);
+        // for (x = 0; x < csvData.length; x++) {
+
+        //     console.log(csvData[x])
+
+        // }
+        //console.log(csvData[0])
+
     })
 
 
